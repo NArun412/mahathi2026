00bdb98dc0e72696ca69fc32d02f53725a02a083
rannamalai <rannamalai-c@globusmedical.com>
2026-01-09 18:10:10 +0530

    News module Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { DigitalAiEnablementComponent } from './pages/healthcare/digital-ai-enab
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { LocationComponent } from './pages/location/location.component';
 import { WorkersCompensiationComponent } from './pages/workers-compensiation/workers-compensiation.component';
+import { NewsComponent } from './pages/news/news.component';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,8 @@ import { WorkersCompensiationComponent } from './pages/workers-compensiation/wor
     LandingPageComponent,
     LocationComponent,
     DigitalAiEnablementComponent,
-    WorkersCompensiationComponent
+    WorkersCompensiationComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
