f490f7cb6c97d47f4e159ce3f31afa2b418d32ee
NArun412 <arunmozhivarathan.n@mahathiinfotech.com>
2026-01-19 15:53:15 +0530

    page banner

--- src/app/pages/aboutus/aboutus.component.ts
+++ src/app/pages/aboutus/aboutus.component.ts
@@ -60,7 +60,7 @@ export class AboutusComponent implements OnInit, AfterViewInit {
        this.registerPageContent();
     setTimeout(() => {
       window.scrollTo({
-        top: 30,
+        top: 0,
         behavior: 'smooth'
       });
     }, 200);
@@ -96,7 +96,7 @@ export class AboutusComponent implements OnInit, AfterViewInit {
       sectionThreeText:
         "A lean & responsive management, a team that’s invested in client’s success, consultants consistently developing subject matter expertise and a leadership that’s focused on quality & timely delivery than selling new services.",
   
-      heroImage: 'assets/images/enhanced_customer_experience.png',
+      heroImage: 'assets/images/abtus.png',
       solutionsHeading: "Streamline operations, Ensure compliance, and Enhance outcomes",
       rightHeroImage:'assets/images/happy_people3.png',
       features: [
