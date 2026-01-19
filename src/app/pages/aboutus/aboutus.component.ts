9bb7432d02434c4b0b5c5cf47e805d09aae5e8b6
NArun412 <arunmozhivarathan.n@mahathiinfotech.com>
2026-01-19 12:59:16 +0530

    banner updated

--- src/app/pages/aboutus/aboutus.component.ts
+++ src/app/pages/aboutus/aboutus.component.ts
@@ -92,9 +92,9 @@ export class AboutusComponent implements OnInit, AfterViewInit {
 
    healthcareData: MainTemplateData = {
       sectionOneText: "",
-      sectionTwoText: 'Healthcare Technology & Digital Services',
+      sectionTwoText: 'Our purpose has always been client centricity',
       sectionThreeText:
-        "Mahathi Infotech partners with healthcare payers, providers, TPAs, and digital health organizations to modernize core operations through scalable technology, data, and AI-led services.",
+        "A lean & responsive management, a team that’s invested in client’s success, consultants consistently developing subject matter expertise and a leadership that’s focused on quality & timely delivery than selling new services.",
   
       heroImage: 'assets/images/enhanced_customer_experience.png',
       solutionsHeading: "Streamline operations, Ensure compliance, and Enhance outcomes",
